4c6bda3bb76ec103d5b3a78885d07c88f560451f
Stefan Seeger <seega@gmx.com>
2022-10-05 22:27:26 +0200

    nx migrate 14.8.3

--- packages/demo/react/jest.config.ts
+++ packages/demo/react/jest.config.ts
@@ -1,10 +1,11 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'demo-react',
   preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.(ts|html)$': 'ts-jest',
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/packages/demo/react',
